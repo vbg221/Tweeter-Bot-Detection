1d4f993e5550d449ce781920a852caa189158834
vbg221 <vbg221@nyu.edu>
2017-05-07 12:22:02 -0400

    Almost final Random Forest. Cleaned Data entirely. Reduced Features to 6.

--- AnalysingData.py
+++ AnalysingData.py
@@ -23,64 +23,143 @@ sheet.column.select([2,3, 6, 7, 8, 10, 11, 12, 14, 18, 19]) # the column indices
 sheet.save_as('C:/Users/vbg22/Downloads/Machine Learning Project/training_data_2_csv_UTF1.csv')
 df = pd.read_csv('C:/Users/vbg22/Downloads/Machine Learning Project/training_data_2_csv_UTF1.csv')
 
+"""
 for i in range(0,len(df)):
-    if(np.isnan(df['screen_name'][i])):
-        df['screen_name'] = 0
-    if(np.isnan(df['location'][i])):
-        df['location'][i] = 0
+    #if(np.isnan(df['screen_name'][i])):
+    #    df['screen_name'] = 0
+    #if(np.isnan(df['location'][i])):
+    #    df['location'][i] = 0
     if(np.isnan(df['followers_count'][i])):
         df['followers_count'][i] = 0
+for i in range(0,len(df)):
     if(np.isnan(df['friends_count'][i])):
         df['friends_count'][i] = 0
+for i in range(0,len(df)):          
     if(np.isnan(df['listedcount'][i])):
         df['listedcount'][i] = 0
+for i in range(0,len(df)):          
     if(np.isnan(df['favourites_count'][i])):
         df['favourites_count'][i] = 0
+for i in range(0,len(df)):          
     if(np.isnan(df['verified'][i])):
         df['verified'][i] = 0
+for i in range(0,len(df)):          
     if(np.isnan(df['statuses_count'][i])):
         df['statuses_count'][i] = 0
-    if(np.isnan(df['status'][i])):
-        df['status'][i] = 0
-    if(np.isnan(df['name'][i])):
-        df['name'][i] = 0
-    
-    if(np.isnull(df['screen_name'][i])):
-        df['screen_name'] = 0
-    if(np.isnull(df['location'][i])):
-        df['location'][i] = 0
-    if(np.isnull(df['followers_count'][i])):
+    #if(np.isnan(df['status'][i])):
+    #    df['status'][i] = 0
+    #if(np.isnan(df['name'][i])):
+    #    df['name'][i] = 0
+
+for i in range(0,len(df)):    
+    if(pd.isnull(df['screen_name'][i])):
+        df['screen_name'][i] = " "
+for i in range(0,len(df)):        
+    if(pd.isnull(df['location'][i])):
+        df['location'][i] = " "
+for i in range(0,len(df)):        
+    if(pd.isnull(df['followers_count'][i])):
         df['followers_count'][i] = 0
-    if(np.isnull(df['friends_count'][i])):
+for i in range(0,len(df)):          
+    if(pd.isnull(df['friends_count'][i])):
         df['friends_count'][i] = 0
-    if(np.isnull(df['listedcount'][i])):
+for i in range(0,len(df)):          
+    if(pd.isnull(df['listedcount'][i])):
         df['listedcount'][i] = 0
-    if(np.isnull(df['favourites_count'][i])):
+for i in range(0,len(df)):          
+    if(pd.isnull(df['favourites_count'][i])):
         df['favourites_count'][i] = 0
-    if(np.isnull(df['verified'][i])):
+for i in range(0,len(df)):          
+    if(pd.isnull(df['verified'][i])):
         df['verified'][i] = 0
-    if(np.isnull(df['statuses_count'][i])):
+for i in range(0,len(df)):          
+    if(pd.isnull(df['statuses_count'][i])):
         df['statuses_count'][i] = 0
-    if(np.isnull(df['status'][i])):
-        df['status'][i] = 0
-    if(np.isnull(df['name'][i])):
-        df['name'][i] = 0
-    
-df = df.dropna()    
-df.to_csv('C:/Users/vbg22/Downloads/Machine Learning Project/out1.csv', sep=',', index=False)
+for i in range(0,len(df)):          
+    if(pd.isnull(df['status'][i])):
+        df['status'][i] = " "
+for i in range(0,len(df)):        
+    if(pd.isnull(df['name'][i])):
+        df['name'][i] = " "
+"""    
+df = df.dropna(0)    
+df.to_csv('C:/Users/vbg22/Downloads/Machine Learning Project/shity.csv', sep=',', index=False)
 
 
 
 
 #import pandas as pd
 
-my_data = pd.read_csv('out1.csv', sep=',')
-my_clean_data = autoclean(my_data)
-my_data.to_csv('shity.csv', sep=',', index=False)
+#my_data = pd.read_csv('out1.csv', sep=',')
+#my_clean_data = autoclean(my_data)
+#my_data.to_csv('shity.csv', sep=',', index=False)
 
 sheet2 = pe.get_sheet(file_name='C:/Users/vbg22/Downloads/Machine Learning Project/Tweeter-Bot-Detection/test_data_4_students.csv', encoding = 'UTF')
 sheet2.column.select([2,3, 6, 7, 8, 10, 11, 12, 14, 18])
 sheet2.save_as('C:/Users/vbg22/Downloads/Machine Learning Project/Tweeter-Bot-Detection/out2.csv')
 my_data2 = pd.read_csv('C:/Users/vbg22/Downloads/Machine Learning Project/Tweeter-Bot-Detection/out2.csv', sep=',')
-cleaned_test = autoclean(my_data2)
-cleaned_test.to_csv('Final_test.csv', sep=',', index=False)
+
+"""
+for i in range(0,len(my_data2)):
+    #if(np.isnan(df['screen_name'][i])):
+    #    df['screen_name'] = 0
+    #if(np.isnan(df['location'][i])):
+    #    df['location'][i] = 0
+    if(np.isnan(my_data2['followers_count'][i])):
+        my_data2['followers_count'][i] = 0
+for i in range(0,len(my_data2)):                
+    if(np.isnan(my_data2['friends_count'][i])):
+        my_data2['friends_count'][i] = 0
+for i in range(0,len(my_data2)):                
+    if(np.isnan(my_data2['listedcount'][i])):
+        my_data2['listedcount'][i] = 0
+for i in range(0,len(my_data2)):                
+    if(np.isnan(my_data2['favourites_count'][i])):
+        my_data2['favourites_count'][i] = 0
+for i in range(0,len(my_data2)):                
+    if(np.isnan(my_data2['verified'][i])):
+        my_data2['verified'][i] = 0
+for i in range(0,len(my_data2)):                
+    if(np.isnan(my_data2['statuses_count'][i])):
+        my_data2['statuses_count'][i] = 0
+    #if(np.isnan(df['status'][i])):
+    #    df['status'][i] = 0
+    #if(np.isnan(df['name'][i])):
+    #    df['name'][i] = 0
+    
+for i in range(0,len(my_data2)):           
+    if(pd.isnull(my_data2['screen_name'][i])):
+        my_data2['screen_name'][i] = " "
+for i in range(0,len(my_data2)):        
+    if(pd.isnull(my_data2['location'][i])):
+        my_data2['location'][i] = " "
+for i in range(0,len(my_data2)):        
+    if(pd.isnull(my_data2['followers_count'][i])):
+        my_data2['followers_count'][i] = 0
+for i in range(0,len(my_data2)):                
+    if(pd.isnull(my_data2['friends_count'][i])):
+        my_data2['friends_count'][i] = 0
+for i in range(0,len(my_data2)):                
+    if(pd.isnull(my_data2['listedcount'][i])):
+        my_data2['listedcount'][i] = 0
+for i in range(0,len(my_data2)):                
+    if(pd.isnull(my_data2['favourites_count'][i])):
+        my_data2['favourites_count'][i] = 0
+for i in range(0,len(my_data2)):                
+    if(pd.isnull(my_data2['verified'][i])):
+        my_data2['verified'][i] = 0
+for i in range(0,len(my_data2)):                
+    if(pd.isnull(my_data2['statuses_count'][i])):
+        my_data2['statuses_count'][i] = 0
+for i in range(0,len(my_data2)):                
+    if(pd.isnull(my_data2['status'][i])):
+        my_data2['status'][i] = " "
+for i in range(0,len(my_data2)):        
+    if(pd.isnull(my_data2['name'][i])):
+        my_data2['name'][i] = " "
+"""    
+df = df.dropna(0)    
+
+
+#cleaned_test = autoclean(my_data2)
+my_data2.to_csv('Final_test.csv', sep=',', index=False)
--- randomForest.py
+++ randomForest.py
@@ -1,4 +1,9 @@
-# -*- coding: utf-8 -*-
+
+# coding: utf-8
+
+# In[1]:
+
+
 """
 Created on Wed Apr  5 20:30:35 2017
 
@@ -20,16 +25,23 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 from datacleaner import autoclean
 
-
-#from sklearn.linear_model import LogisticRegression
-
-
-data = pd.read_csv('shity.csv') #, encoding = 'latin1'
-data2 = pd.read_csv('Final_test.csv')
+data = pd.read_csv('C:/Users/vbg22/Downloads/Pictures/binary and continouous.csv') #, encoding = 'latin1'
+#data = pd.read_csv('C:/Users/vbg22/Downloads/Pictures/binary and continouous.csv', encoding = 'latin1') #, encoding = 'latin1'
+data2 = pd.read_csv('C:/Users/vbg22/Downloads/Machine learning Project/Tweeter-Bot-Detection/test2.csv', encoding = 'latin1')
 data = data.dropna()
-#data2.dropna() 
+data2.dropna()
 data = data.replace(np.nan, '', regex = True)
 data2 = data2.replace(np.nan, '', regex = True)
+data
+
+
+# In[2]:
+
+data.shape
+
+
+# In[3]:
+
 """
 for i in range(0,len(data)):
     if(np.isnan(data['screen_name'][i])):
@@ -46,10 +58,10 @@ for i in range(0,len(data)):
 #np.isnan(data.any()) #and gets False
 #np.isfinite(data.all()) #and gets True
             
-data.replace(to_replace = True, value = 1)
-data.replace(to_replace = False, value = 0)
-data2.replace(to_replace = True, value = 1)
-data2.replace(to_replace = False, value = 0)
+#data.replace(to_replace = True, value = 1)
+#data.replace(to_replace = False, value = 0)
+#data2.replace(to_replace = True, value = 1)
+#data2.replace(to_replace = False, value = 0)
 #data2.replace(to_replace = Nan, value = 0)
 #data2.replace(to_replace = "", value = '')
 
@@ -58,8 +70,13 @@ data2.replace(to_replace = False, value = 0)
 
 #type(data.ix[0]['screen_name'])
 #data['screen_name'].astype(float)
-data.astype(float)
-data2.astype(float)
+#data['name'].astype(float)
+#data2['screen_name'].astype(float)
+#data.astype(float)
+#data2.astype(float)
+
+#data = autoclean(data)
+#data2 = autoclean(data2)
 
 data.dropna()
 data2.dropna()
@@ -74,48 +91,146 @@ data=data.description.str.contains(pat)*1
 
 #msk = np.random.rand(len(data)) < 0.8
          
-                    
+
+#,'url','created_at','lang','status','favourites_count','friends_count'
+cols = ['screen_name','location','description','followers_count','listedcount']
+colRes = ['bot']                    
                      
 train = data
 test = data2
 
-train.dropna()
-test.dropna()
+train = train[cols]
+test = test[cols]
+test
+
+
+# In[4]:
+
 #test = test.replace(np.nan, '', regex = True)
+#data = autoclean(data)
+#test = autoclean(test)
+test
 
-cols = ['screen_name','location','followers_count','friends_count','listedcount','favourites_count','verified','statuses_count','status','name']
-colRes = ['bot']
 
+# In[ ]:
+
+
+
+
+# In[5]:
+
+for header_name in cols:
+    temp = list()
+    for i in range(len(train)):
+        temp.append(train.iloc[i][header_name].astype(float))
+    train[header_name]= temp
+print(train.shape)
+train
+
+
+# bigtemp = list()
+# for header_name in cols:
+#     temp = list()
+#     for i in range(len(test)):
+#         temp.append(test[header_name][i])
+#     bigtemp.append(temp)
+# 
+# bigtemp = autoclean(bigtemp)
+# j = 0
+# for header_name in cols:
+#     for i in range(len(test)):
+#         test[header_name][i] = bigtemp[j][i]
+#     j = j + 1
+
+# In[6]:
+
+for header_name in cols:
+    temp = list()
+    for i in range(len(test)):
+        temp.append(test.iloc[i][header_name].astype(float))
+    test[header_name]= temp
+print(test.shape)
+test
+
+
+# In[7]:
+
+train =train.dropna()
+train.shape
+
+
+# In[8]:
+
+train2 = data[colRes].astype(int)
 trainArr = train.as_matrix(cols) #training array
 #trainArr = trainArr.replace(np.nan, 0, regex = True)
-trainRes = train.as_matrix(colRes) # training results
-                         
-rf = RandomForestClassifier(n_estimators=100) # initialize
+trainRes = train2.as_matrix(colRes) # training results
+traindata=trainArr.astype(int)
+traindata
+
+
+# In[9]:
+
+
+rf = RandomForestClassifier(n_estimators = 100) # initialize
 rf.fit(trainArr, trainRes) # fit the data to the algorithm
+
+
+# In[10]:
+
+
+
+#test = test[cols]
+
+
+# In[11]:
 
 testArr = test.as_matrix(cols)
 #testArr = testArr.replace(np.nan, 0, regex = True)
-results = rf.predict(test)
-#results.toColumn()
+testArr
 
-df = pd.read_csv('test_data_4_students.csv')
+
+# In[12]:
+
+test.shape
+
+
+# In[13]:
+
+results = rf.predict(testArr)
+#results.toColumn()
+results
+
+
+# In[14]:
+
+df = pd.read_csv('C:/Users/vbg22/Downloads/Machine learning Project/Tweeter-Bot-Detection/test_data_4_students.csv')
 df2 = df['id']
 df2 = pd.concat([df['id'],pd.DataFrame(results.T)], axis=1, join='inner')
 
 df2.columns = ['Id','Bot']
 
-df2.dropna()
+
+# In[15]:
+
+#df2.dropna()
 df2['Id'].astype(int)
-df2.to_csv('Output.csv', sep=',', index=False)
+df2.to_csv('C:/Users/vbg22/Downloads/Machine learning Project/Tweeter-Bot-Detection/Output.csv', sep=',', index=False)
 #df2['bot'] = rf.predict(testArr)
 #np.shape(df2)
 #test['bot'] = results
 #test.head(10)
 
-'''
-#rf.score(test['bot'], test['predictions'])
-rf.score(testArr, test['bot'])
 
+# In[16]:
+
+#rf.score(test['bot'], test['predictions'])
+Ans = pd.read_csv('C:/Users/vbg22/Downloads/Pictures/sub6.csv', encoding = 'UTF')
+Pred = pd.read_csv('C:/Users/vbg22/Downloads/Machine learning Project/Tweeter-Bot-Detection/Output.csv', encoding = 'UTF')
+#rf.score(Pred['Bot'], Ans['Bot'])
+
+
+# In[17]:
 
 def calculateAccuracy(bot, predictions, length):
     tp = 0
@@ -136,15 +251,24 @@ def calculateAccuracy(bot, predictions, length):
     
     return [tp, fp, tn, fn]
 
-len(test)
+len(Pred)
+len(Ans)
+Pred
 
-truePositive, falsePositive, trueNegative, falseNegative = calculateAccuracy(test['bot'], test['predictions'], len(test))
+
+# In[18]:
+
+truePositive, falsePositive, trueNegative, falseNegative = calculateAccuracy(Ans['Bot'], Pred['Bot'], 575)
 tpr = (truePositive) / (truePositive + falseNegative)
 fpr = (falsePositive) / (falsePositive + trueNegative)
 
 tpr = [0.0, tpr, 1.0]
 fpr = [0.0, fpr, 1.0]
 
+
+# In[19]:
+
+"""
 plt.title('Receiver Operating Characteristic')
 plt.plot(fpr, tpr, 'b')
 plt.legend(loc = 'lower right')
@@ -154,13 +278,58 @@ plt.ylim([0, 1])
 plt.ylabel('True Positive Rate')
 plt.xlabel('False Positive Rate')
 plt.show()
-
-print(classification_report(test['bot'], test['predictions']))
-'''
 """
-# load dataset
-dta = sm.datasets.fair.load_pandas().data
 
-# add "affair" column: 1 represents having affairs, 0 represents not
-dta['affair'] = (dta.affairs > 0).astype(int)
-"""
+
+# In[20]:
+
+from sklearn.metrics import accuracy_score
+accuracy_score(Ans['Bot'],Pred['Bot'])
+
+
+# In[21]:
+
+print(classification_report(Ans['Bot'], Pred['Bot']))
+
+
+# In[22]:
+
+from sklearn.datasets import make_classification
+from sklearn.ensemble import ExtraTreesClassifier
+
+
+# In[23]:
+
+importances = rf.feature_importances_
+std = np.std([tree.feature_importances_ for tree in rf.estimators_],
+             axis=0)
+indices = np.argsort(importances)[::-1]
+
+
+# In[24]:
+
+print("Feature ranking:")
+
+for f in range(train.shape[1]):
+    print("%d. feature %s (%f)" % (f + 1, cols[f], importances[indices[f]]))
+
+# Plot the feature importances of the forest
+plt.figure()
+plt.title("Feature importances")
+plt.bar(range(train.shape[1]), importances[indices],
+       color="r", yerr=std[indices], align="center")
+plt.xticks(range(train.shape[1]), indices)
+plt.xlim([-1, train.shape[1]])
+plt.show()
+
+
+# In[25]:
+
+for f in range(0,train.shape[1]):
+    print("%d. feature %s (%f)" % (f + 1, cols[f], importances[indices[f]]))
+
+
+# In[26]:
+
+train.shape[1]
+
